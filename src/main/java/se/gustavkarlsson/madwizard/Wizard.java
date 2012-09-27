48c7ccc508ce11e0a17d0cd517931fed0912b234
Gustav Karlsson <gustav.karlsson@gmail.com>
2012-09-27 19:23:16 +0200

    Added Javadoc to Wizard.java

--- src/main/java/se/gustavkarlsson/madwizard/Wizard.java
+++ src/main/java/se/gustavkarlsson/madwizard/Wizard.java
@@ -4,14 +4,43 @@ import java.awt.Container;
 
 import javax.swing.AbstractButton;
 
+/**
+ * An interface for creating wizards.
+ * 
+ * @author Gustav
+ */
 public interface Wizard {
 
+	/**
+	 * Gets the container where {@link WizardPage}s are contained. This is used by the {@link WizardController} to
+	 * (un)set wizard pages.
+	 * 
+	 * @return the {@link WizardPage} container
+	 */
 	Container getWizardPageContainer();
 
+	/**
+	 * Gets the navigation button for "previous". This is used by the {@link WizardController} to navigate back in the
+	 * wizard.
+	 * 
+	 * @return the "previous" button
+	 */
 	AbstractButton getPreviousButton();
 
+	/**
+	 * Gets the navigation button for "next". This is used by the {@link WizardController} to navigate forward in the
+	 * wizard. Also needed so that the {@link WizardController} can enable/disable the button.
+	 * 
+	 * @return the "next" button
+	 */
 	AbstractButton getNextButton();
 
+	/**
+	 * Gets the navigation button for "finish". This is needed so that the {@link WizardController} can enable/disable
+	 * the button.
+	 * 
+	 * @return the "finish" button
+	 */
 	AbstractButton getFinishButton();
 
 }
