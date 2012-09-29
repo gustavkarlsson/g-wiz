a5e35eecd9e46064ba54575d906e75851b90ed29
Gustav Karlsson <gustav.karlsson@gmail.com>
2012-09-29 20:07:40 +0200

    Added license information to all source files

--- src/main/java/se/gustavkarlsson/gwiz/DefaultWizard.java
+++ src/main/java/se/gustavkarlsson/gwiz/DefaultWizard.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2012 Gustav Karlsson <gustav.karlsson@gmail.com>.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Public License v3.0
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/gpl.html
+ * 
+ * Contributors:
+ *     Gustav Karlsson <gustav.karlsson@gmail.com> - initial API and implementation
+ ******************************************************************************/
 package se.gustavkarlsson.gwiz;
 
 import java.awt.Dimension;
